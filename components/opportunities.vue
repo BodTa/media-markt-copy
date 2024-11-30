bfc2d40dac652d64071e63125594fdbbb22cfa7d
alicanyldrm1907 <yildirimalican3347@gmail.com>
2024-11-30 21:18:17 +0300

    opportunities

--- components/opportunities.vue
+++ components/opportunities.vue
@@ -1,7 +1,60 @@
 <template>
-  <div>Fırsatlar</div>
+  <div class="opportunities">
+    <div class="opportunities-container">
+      <div class="opportunity-item">
+        <img src="/public/images/MobilUygulamamiziKesfedin.png" alt="" />
+        <p class="opportunity-subtext">Mobil Uygulamamızı Keşfedin!</p>
+      </div>
+      <div class="opportunity-item">
+        <img src="/public/images/CLUBPuan.png" alt="" />
+        <p class="opportunity-subtext">CLUB Puan</p>
+        </div>
+      <div class="opportunity-item">
+        <img src="/public/images/IsBankasi.png" alt="" />
+        <p class="opportunity-subtext">İş Bankası</p>
+      </div>
+      <div class="opportunity-item">
+        <img src="/public/images/YapiKredi.png" alt="" />
+        <p class="opportunity-subtext">Yapı Kredi</p>
+      </div>
+      <div class="opportunity-item">
+        <img src="/public/images/9aVaranTaksitFirsati1.png" alt="" />
+        <p class="opportunity-subtext">9'a Varan Taksit Fırsatı!</p>
+      </div>
+      <div class="opportunity-item">
+        <img src="/public/images/9aVaranTaksitFirsati2.png" alt="" />
+        <p class="opportunity-subtext">9'a Varan Taksit Fırsatı!</p>
+      </div>
+    </div>
+    
+  </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style></style>
+<style>
+.opportunities {
+  padding: 16px;
+  background-color: black;
+}
+
+.opportunities-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  gap: 5px;
+}
+
+.opportunity-item:hover {
+  text-decoration: underline;
+  text-decoration-color: white;
+  border-radius: 10px;
+  background-color: rgb(34, 34, 34);
+}
+
+.opportunity-subtext {
+  text-align: center;
+  font-size: 15px;
+  color: rgb(156, 156, 156);
+}
+</style>
