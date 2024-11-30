f47291d14673a38dedc7f73d744ffac5264fa22c
alicanyldrm1907 <yildirimalican3347@gmail.com>
2024-11-30 18:23:05 +0300

    ourServicesImages

--- components/ourServices.vue
+++ components/ourServices.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>Hizmetlerimiz</div>
+  <div class="ourservices">
+    <p class="ourservices-text">Hizmetlerimiz</p>
+    <img src="/public/images/MagazadanTeslimAl.png" alt="" />
+  </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style></style>
+<style>
+.ourservices {
+  padding: 16px;
+}
+
+.ourservices-text {
+  padding: 20px;
+  font-size: 40px;
+  color: white;
+}
+</style>
