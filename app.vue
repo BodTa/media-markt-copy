12f5201213b759582daa9d0c340967c10739ff39
Taner Özdemir <95244060+BodTa@users.noreply.github.com>
2025-01-17 21:03:16 +0300

    update

--- app.vue
+++ app.vue
@@ -1,19 +1,19 @@
 <template>
   <navigationBar
-    :allCategoriesOpen="allCategoriesOpen"
-    :marketSearchOpen="marketSearchOpen"
+    :toggleAllCategories="() => (allCategoriesOpen = !allCategoriesOpen)"
+    :toggleMarketSearch="() => (marketSearchOpen = !marketSearchOpen)"
   />
   <NuxtPage />
   <footerBar />
   <div
     v-if="allCategoriesOpen"
-    class="fixed inset-0 bg-black bg-opacity-50 z-40"
+    class="fixed inset-0 bg-black bg-opacity-50 z-40 pointer-events-none"
   >
     <div class="w-[25vw] h-full bg-white"></div>
   </div>
   <div
     v-if="marketSearchOpen"
-    class="fixed inset-0 bg-black bg-opacity-50 z-40"
+    class="fixed inset-0 bg-black bg-opacity-50 z-40 pointer-events-none"
   >
     <div class="w-[25vw] h-full bg-white"></div>
   </div>
