4222d1394ccfe383d11b0db3a5fdb7d704cbabed
alicanyldrm1907 <yildirimalican3347@gmail.com>
2025-01-19 23:18:16 +0300

    categories

--- stores/categories.ts
+++ stores/categories.ts
@@ -1,7 +1,17 @@
 import { defineStore } from 'pinia'
+import type CategoryRecommendation from '~/models/categoryRecommendation';
 
 export const useMyCategoriesStore = defineStore({
   id: 'myCategoriesStore',
   state: () => ({ }),
-  actions: {}
+  actions: {
+    async fetchPicks() {
+      const { $db } = useNuxtApp();
+      const querySnapshot = await getDocs(collection($db, "categoryRecommendation"));
+
+      this.picks = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      })) as CategoryRecommendation[];
+  }
 })
