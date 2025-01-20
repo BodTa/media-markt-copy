5d6ec5476bf23d6ac1676c4c7021f4fb4b8feb6b
alicanyldrm1907 <yildirimalican3347@gmail.com>
2025-01-20 15:13:28 +0300

    firebase

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 export default defineNuxtPlugin((nuxtApp) => {
 
 const firebaseConfig = {
@@ -15,10 +16,13 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
+const auth = getAuth(app);
+
 return {
   provide: {
     firebaseApp: app,
     db,
+    auth,
   },
 };
 });
